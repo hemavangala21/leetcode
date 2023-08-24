7f786e82c5788621be26e6aa6382c59598664881
hemavangala21 <124653009+hemavangala21@users.noreply.github.com>
2023-08-24 09:13:16 +0530

    Time: 1 ms (98.45%), Space: 44.2 MB (64.52%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int l=flowerbed.length;
+        int count=0;
+
+        if(n==0){
+            return true;
+        }
+
+        if(l==1){
+            if(flowerbed[0]==0)
+            return true;
+
+            else
+            return false;
+        }
+
+        if(flowerbed[0]==0 && flowerbed[1]==0){
+            count++;
+            flowerbed[0]=1;
+        }
+
+        for(int i=1;i<flowerbed.length-1;i++){
+            if(flowerbed[i] == 0){
+                if(flowerbed[i-1]==0 && flowerbed[i+1]==0){
+                    flowerbed[i]=1;
+                    count++;
+                }
+            }
+        }
+
+       
+
+        if(flowerbed[l-1]==0 && flowerbed[l-2] == 0){
+            count++;
+        }
+        if(count>=n)
+        return true;
+        
+        return false;
+        
+    }
+}
