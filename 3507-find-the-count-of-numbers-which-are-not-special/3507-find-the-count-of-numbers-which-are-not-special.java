c2c77f68e71bae5d1cf2677829a14d97a3da8c4a
hemavangala21 <124653009+hemavangala21@users.noreply.github.com>
2024-07-28 09:50:26 +0530

    Time: 16 ms (100%), Space: 44.9 MB (100%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
@@ -1,32 +1,28 @@
 class Solution {
     public int nonSpecialCount(int l, int r) {
-       
-        int maxLimit = (int)Math.sqrt(r);
-        boolean[] isPrime = new boolean[maxLimit + 1];
+        int max = (int)Math.sqrt(r);
+        boolean[] isPrime = new boolean[max + 1];
         Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
-        
-        for (int i = 2; i * i <= maxLimit; i++) {
+        isPrime[0]= isPrime[1] = false;
+        for (int i=2; i*i <= max; i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j <= maxLimit; j += i) {
+                for (int j=i * i; j<= max; j+= i) {
                     isPrime[j] = false;
                 }
             }
-        }
-        
-      
+        }  
         List<Integer> primeSquares = new ArrayList<>();
-        for (int i = 2; i <= maxLimit; i++) {
+        for (int i= 2; i<= max;i++) {
             if (isPrime[i]) {
-                long square = (long)i * i;
-                if (square >= l && square <= r) {
+                long square=(long)i * i;
+                if (square>= l && square<= r) {
                     primeSquares.add((int)square);
                 }
             }
         }
         
-        int totalNumbers = r - l + 1;
+        int totalNumbers = r-l + 1;
         int specialNumbers = primeSquares.size();
-        return totalNumbers - specialNumbers;
+        return totalNumbers-specialNumbers;
     }
 }
