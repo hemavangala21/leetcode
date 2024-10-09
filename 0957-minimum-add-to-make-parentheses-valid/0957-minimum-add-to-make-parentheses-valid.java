51d347e1f3085fcda9a8b7e6871e56df2fd3e52b
Vangala Hemalatha <124653009+hemavangala21@users.noreply.github.com>
2024-10-09 19:39:09 +0530

    Time: 0 ms (100%), Space: 41.4 MB (64.62%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int mismatch = 0;
+
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                open++;
+            }
+            else{
+                if(open>0){
+                    open--;
+                }
+                else{
+                    mismatch++;
+                }
+            }
+        }
+        return open+mismatch;
+    }
+}
