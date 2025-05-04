ce0c78a07b3d8b497571db2b762d074caa63523b
Vangala Hemalatha <124653009+hemavangala21@users.noreply.github.com>
2025-05-04 23:33:00 +0530

    Time: 1 ms (63.32%), Space: 44.6 MB (95.95%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,33 +1,11 @@
-/*class Solution {
-    public int removeDuplicates(int[] nums) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            if(i<nums.length-1 && nums[i]==nums[i+1]){
-                continue;
-            }
-            else{
-                nums[count]=nums[i];
-                count++;
+class Solution {
+    public int removeDuplicates(int[] nums) {int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
             }
         }
-        return count;
+        return j;
     }
 }
-
-*/
-
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int count =0;
-        for(int i=0;i<nums.length;i++){
-            if(i<nums.length-1 && nums[i]==nums[i+1]){
-                continue;
-            }
-            else{
-                nums[count]=nums[i];
-                count++;
-            }
-        }
-        return count;
-    }
-}        
